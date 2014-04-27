6527fc1f7fa22a72062d2b26baac5308c83e9b6d
Dheena <dheenaindian@gmail.com>
2014-04-27 17:34:48 +0530

    code cleanup

--- lib/hierarchy/added_hierarchy_level.rb
+++ lib/hierarchy/added_hierarchy_level.rb
@@ -4,13 +4,16 @@ module AddedHierarchyLevel
   @@linear = []
 
   def added_hierarchy(result_set,args,depth = 0)
-    initial = initial_load(result_set,args)
-    level_check(depth) ? initial.tree_structure(result_set,@@result_group) : initial.tree_structure_depth(result_set,@@result_group,depth.to_i)
+    added_hierarchy_calc(result_set,args,depth,'tree_structure')
   end
  
   def added_hierarchy_level(result_set,args,depth = 0)
+    added_hierarchy_calc(result_set,args,depth,'tree_structure_level')
+  end
+  
+  def added_hierarchy_calc(result_set,args,depth,type)
     initial = initial_load(result_set,args)
-    level_check(depth) ? initial.tree_structure_level(result_set,@@result_group) : initial.tree_structure_depth_level(result_set,@@result_group,depth.to_i)
+    method_detection(result_set,args,depth,type,self,@@result_group)
   end
 
   def added_hchildren(result_set,args)
@@ -24,29 +27,37 @@ module AddedHierarchyLevel
   end
 
   def linear_level(result_set,depth = 0)
-    result_group = linear_load(result_set)
-    level_check(depth) ? self.linear_structure_level(result_set,result_group) : self.linear_structure_depth_level(result_set,result_group,depth.to_i)
-    return @@linear
+    linear_calc(result_set,depth,'linear_structure_level')
   end
 
   def linear(result_set,depth = 0)
-    result_group = linear_load(result_set)
-    level_check(depth) ? self.linear_structure(result_set,result_group) : self.linear_structure_depth(result_set,result_group,depth.to_i)
-    return @@linear
+    linear_calc(result_set,depth,'linear_structure')
   end
 
-  def added_linear(result_set,args,depth = 0)
-    initial = added_linear_load(result_set,args)
-    level_check(depth) ? initial.linear_structure(result_set,@@result_group) : initial.linear_structure_depth(result_set,@@result_group,depth.to_i)
-    return @@linear
-  end
-  
-  def added_linear_level(result_set,args,depth = 0)
-    initial = added_linear_load(result_set,args)
-    level_check(depth) ? initial.linear_structure_level(result_set,@@result_group) : initial.linear_structure_depth_level(result_set,@@result_group,depth.to_i)
+  def linear_calc(result_set,depth,type)  
+    result_group = linear_load(result_set)
+    method_detection(result_set,args,depth,type,self,result_group)
     return @@linear
   end
  
+  def added_linear(result_set,args,depth = 0)
+    added_calc(result_set,args,depth,'linear_structure')
+  end
+  
+  def added_linear_level(result_set,args,depth = 0)
+    added_calc(result_set,args,depth,'linear_structure_level')
+  end
+
+  def added_calc(result_set,args,depth,type)
+    initial = added_linear_load(result_set,args)
+    method_detection(result_set,args,depth,type,initial,@@result_group)
+    return @@linear
+  end 
+  
+  def method_detection(result_set,args,depth,type,initial,result_group)
+    level_check(depth) ? initial.send(type,result_set,result_group) : initial.send(type+'_depth',result_set,@@result_group,depth.to_i)
+  end
+
   def added_linear_load(result_set,args)   
     @@linear = initialise_array()
     initial_load(result_set,args)
@@ -73,10 +84,10 @@ module AddedHierarchyLevel
     result_group[self.id].map{|x| x.linear_structure_level(result_set,result_group,increment+1)} unless presence_check(result_group[self.id])
   end
 
-  def linear_structure_depth_level(result_set,result_group,depth,increment = 1)
+  def linear_structure_level_depth(result_set,result_group,depth,increment = 1)
     unless compare(increment,depth)
       @@linear << self.attributes.merge({:level => increment})
-      result_group[self.id].map{|x| x.linear_structure_depth_level(result_set,result_group,depth,increment + 1)} unless presence_check(result_group[self.id]) 
+      result_group[self.id].map{|x| x.linear_structure_level_depth(result_set,result_group,depth,increment + 1)} unless presence_check(result_group[self.id]) 
     end
   end
 
--- lib/hierarchy/hierarchy_with_level.rb
+++ lib/hierarchy/hierarchy_with_level.rb
@@ -2,15 +2,15 @@ module HierarchyWithLevel
 
   def hierarchy_level(result_set,depth = 0)
     result_group = group_result_set(result_set)
-    level_check(depth) ? self.tree_structure_level(result_set,result_group) : self.tree_structure_depth_level(result_set,result_group,depth.to_i)
+    level_check(depth) ? self.tree_structure_level(result_set,result_group) : self.tree_structure_level_depth(result_set,result_group,depth.to_i)
   end
 
   def tree_structure_level(result_set,result_group,increment = 1)
     presence_check(result_group[self.id]) ? self.attributes.merge({:level => increment}) : self.attributes.merge({:level => increment, :children => result_group[self.id].map{|x| x.tree_structure_level(result_set,result_group,increment + 1)}}) 
   end
 
-  def tree_structure_depth_level(result_set,result_group,depth,increment = 1)
-    presence_check(result_group[self.id]) ? self.attributes.merge({:level => increment}) : self.attributes.merge({:level => increment, :children => result_group[self.id].map{|x| x.tree_structure_depth_level(result_set,result_group,depth,increment + 1)}}) unless compare(increment,depth)
+  def tree_structure_level_depth(result_set,result_group,depth,increment = 1)
+    presence_check(result_group[self.id]) ? self.attributes.merge({:level => increment}) : self.attributes.merge({:level => increment, :children => result_group[self.id].map{|x| x.tree_structure_level_depth(result_set,result_group,depth,increment + 1)}}) unless compare(increment,depth)
   end
 
   def hchildren(result_set)
